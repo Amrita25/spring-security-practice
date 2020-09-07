674720f7c91086e1257348085ef29ac43a810409
Amrita25 <amrita.majumdar25@gmail.com>
2020-09-07 22:13:46 +0530

    first commit

---
+++ src/test/java/io/springboot/security/practice/springsecuritypractice/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springboot.security.practice.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
